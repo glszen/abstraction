980270e933607343b1b17030c2c690ce41cfe066
Gülşen <gulsenk48@hotmail.com>
2024-10-23 22:27:54 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using abstraction;
+
+ employees Employe1 = new projectManager { Name = "Hasan", Surname = "Çıldırmış", Department = "Project Manager" };
+
+Employe1.Info();
+Employe1.Mission();
+
+employees Employe2 = new softwareDeveloper { Name = "Emily", Surname = "Cooper", Department = "Software Developer" };
+
+Employe2.Info();
+Employe2.Mission();
+
+employees Employe3 = new salesRepresentative { Name = "Mindy", Surname = "Chen", Department = "Sales Represantative" };
+
+Employe3.Info();
+Employe3.Mission();
---
+++ employees.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstraction
+{
+    public abstract class employees
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Department { get; set; }
+
+        public abstract void Mission();
+       
+        public void Info()
+        {
+            Console.WriteLine($"{Name} {Surname} Department is: {Department}");
+        }
+    }
+
+    public class projectManager : employees 
+    { 
+        public override void Mission()
+        {
+            Console.WriteLine("I work as a project manager.\n");
+        }
+    } 
+    
+    
+    public class softwareDeveloper : employees 
+    { 
+        public override void Mission()
+        {
+            Console.WriteLine("I work as a sofware developer.\n");
+        }
+    }
+
+    public class salesRepresentative : employees
+    {
+        public override void Mission()
+        {
+            Console.WriteLine("I work as a sales representative.\n");
+        }
+    }
+
+
+}
